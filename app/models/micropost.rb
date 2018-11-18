f0d47d651a66f2dc5521b3ee23e3b8b8ab4913df
satoryt <sato@ipsound.net>
2018-11-18 12:31:00 -0800

    Finish the toy app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,8 @@
 class Micropost < ApplicationRecord
+
+
+    validates :content, length: { maximum: 140 }, presence: true
+    belongs_to :user
+
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+
+    has_many :microposts
+    validates :name, presence: true
+    validates :email, presence: true
+
+    
 end
